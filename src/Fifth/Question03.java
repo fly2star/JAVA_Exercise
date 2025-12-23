bcdddbac0909af31c64d8d31b0db3dfbddcc6f20
fly2star <2455741679@qq.com>
2025-12-23 15:52:36 +0800

    1223

--- src/Fifth/Question03.java
+++ src/Fifth/Question03.java
@@ -5,19 +5,30 @@ package Fifth;
  * @version 1.0
  * create 2025-12-10-21:24
  **/
+/*
+712. 两个字符串的最小ASCII删除和
+给定两个字符串s1 和 s2，返回 使两个字符串相等所需删除字符的 ASCII 值的最小和 。
+*/
 public class Question03 {
     public static void main(String[] args) {
-
+        String s1 = "sea";
+        String s2 = "eat";
+        System.out.println(minimumDeleteSum(s1, s2));
     }
 
-    public static int f712(String s1, String s2) {
+    // 方法1:动态规划
+    public static int minimumDeleteSum(String s1, String s2) {
         int m = s1.length(), n = s2.length();
+        // dp[i][j] 表示 s1[0:i] 和 s2[0:j] 的最小ASCII删除和
         int[][] dp = new int[m + 1][n + 1];
+        // 边界情况
+        // 当 i=j=0 时, s1[0:i]和s2[0:j]都为空, dp[i][j] = 0
         // j=0 的情况: s2 为空串, 删除 s1 中所有的字符串
+        // i=0 的情况: s1 为空串, 删除 s2 中所有的字符串
         for (int i = 1; i <= m; i++) {
             dp[i][0] = dp[i - 1][0] + s1.codePointAt(i - 1);
         }
-        // i=0 的情况: s1 为空串, 删除 s2 中所有的字符串
+
         for (int j = 1; j <= n; j++) {
             dp[0][j] = dp[0][j - 1] + s2.codePointAt(j - 1);
         }
--- src/Sixth/Question01.java
+++ src/Sixth/Question01.java
@@ -8,12 +8,18 @@ import java.util.List;
  * @version 1.0
  * create 2025-12-15-09:56
  **/
+/*
+2110. 股票平滑下跌阶段的数目
+给你一个整数数组 prices ，表示一支股票的历史每日股价，其中 prices[i] 是这支股票第 i 天的价格。
+一个 平滑下降的阶段 定义为：对于 连续一天或者多天 ，每日股价都比 前一日股价恰好少 1 ，这个阶段第一天的股价没有限制。
+请你返回 平滑下降阶段 的数目
+*/
 public class Question01 {
     public static void main(String[] args) {
         int[] prices = {3, 2, 1, 4};
         int[] prices2 = {8, 6, 7, 7};
         System.out.println(getDescentPeriods( prices));
-        System.out.println(getDescentPeriods( prices2));
+        System.out.println(getDescentPeriods2( prices2));
     }
 
     public static long getDescentPeriods(int[] prices) {
@@ -32,7 +38,7 @@ public class Question01 {
             i = j + 1;
         }
         for (int num : smoothArr) {
-            System.out.println(num);
+//            System.out.println(num);
             res += (long)num * (num + 1) / 2;
         }
         return res;
