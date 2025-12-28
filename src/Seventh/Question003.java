0bc4480848e843e66c6667611af756842a505b04
fly2star <2455741679@qq.com>
2025-12-28 21:00:12 +0800

    1228-1st

---
+++ src/Seventh/Question003.java
@@ -0,0 +1,34 @@
+package Seventh;
+
+/**
+ * @author ccwwll
+ * @version 1.0
+ * create 2025-12-28-20:49
+ **/
+/*
+1351. 统计有序矩阵中的负数
+给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非严格递减顺序排列。 请你统计并返回 grid 中 负数 的数目。
+*/
+public class Question003 {
+    public static void main(String[] args) {
+        int[][] grid = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
+        System.out.println(countNegatives(grid));
+    }
+
+    public static int countNegatives(int[][]  grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int count = 0;
+                if (grid[i][j] < 0) {
+                    count = n - j;
+                    ans += count;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
--- src/Seventh/Question02.java
+++ src/Seventh/Question02.java
@@ -5,7 +5,18 @@ package Seventh;
  * @version 1.0
  * create 2025-12-26-16:18
  **/
-public class Question020 {
+/*
+2483. 商店的最少代价
+给你一个顾客访问商店的日志，用一个下标从 0 开始且只包含字符 'N' 和 'Y' 的字符串 customers 表示：
+    如果第 i 个字符是 'Y' ，它表示第 i 小时有顾客到达。
+    如果第 i 个字符是 'N' ，它表示第 i 小时没有顾客到达。
+如果商店在第 j 小时关门（0 <= j <= n），代价按如下方式计算：
+    在开门期间，如果某一个小时没有顾客到达，代价增加 1 。
+    在关门期间，如果某一个小时有顾客到达，代价增加 1 。
+请你返回在确保代价 最小 的前提下，商店的 最早 关门时间。
+注意，商店在第 j 小时关门表示在第 j 小时以及之后商店处于关门状态。
+*/
+public class Question02 {
     public static void main(String[] args) {
         String customers = "YYYY";
         System.out.println(bestClosingTime(customers));
