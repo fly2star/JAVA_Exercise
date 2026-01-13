3ef6f315571822b907cfa82bdc347c7f59613790
fly2star <2455741679@qq.com>
2026-01-13 18:35:28 +0800

    0113-2cd

--- src/DailyQuestion/Tenth/Question02.java
+++ src/DailyQuestion/Tenth/Question02.java
@@ -1,6 +1,5 @@
 package DailyQuestion.Tenth;
 
-import java.awt.*;
 import java.util.*;
 import java.util.List;
 
@@ -12,10 +11,11 @@ import java.util.List;
 public class Question02 {
     public static void main(String[] args) {
         int[][] squares = {{0, 0, 2}, {1, 1, 1}};
-        System.out.println(f3453(squares));
+        System.out.println(separateSquares2(squares));
     }
 
-    public static double f3453(int[][] squares) {
+    // 方法1: 结果只能是自然数
+    public static double separateSquares(int[][] squares) {
         int n = squares.length;
         double ans = 0d;
         // 提取所有的y坐标
@@ -58,6 +58,71 @@ public class Question02 {
 
         return yCoords.get(yCoords.size() - 1);
 
+    }
+
+    // 方法2: 参考二分法
+    public static double separateSquares2(int[][] squares) {
+        double low = Double.MAX_VALUE;
+        double high = Double.MIN_VALUE;
+
+        // 确定二分的边界
+        for (int[] square : squares) {
+            low = Math.min(low, (double)square[1]);
+            high = Math.max(high, (double)square[1] + square[2]);
+        }
+
+        // 二分查找 y 坐标, 先试着循环 100 次
+        for (int i = 0; i < 100; i++) {
+            double mid = (low + high) / 2.0d;
+            if (calAboveArea(squares, mid) > calBelowArea(squares, mid)) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+
+    }
+
+    public static double calAboveArea(int[][] squares, double y) {
+    /**
+     * 计算 y 上方区域面积
+     * */
+        double area = 0.0d;
+        for (int[] square : squares) {
+            double yi = square[1];
+            double li = square[2];
+            double top = yi + li;
+            if (top <= y ) continue;  // 完全在 y 下方
+            if (yi >= y) {
+                area += li * li;    // 完全在 y 上方
+            } else {
+                area += (top - y) * li;
+            }
+        }
+
+        return area;
+
+    }
+
+    public static double calBelowArea(int[][] squares, double y) {
+        /**
+         * 计算 y 下方区域面积
+         * */
+        double area = 0.0d;
+        for (int[] square : squares) {
+            double yi = square[1];
+            double li = square[2];
+            double top = yi + li;
+            if (yi >= y ) continue;  // 完全在 y 上方
+            if (top <= y) {
+                area += li * li;    // 完全在 y 上方
+            } else {
+                area += (y - yi) * li;
+            }
+        }
+
+        return area;
 
     }
 }
