ec9ab83a26cc673b73ad9bdf00c812846bdea290
fly2star <2455741679@qq.com>
2026-01-27 15:22:07 +0800

    0127-1st

--- src/LeetCode_Hot100/question031.java
+++ src/LeetCode_Hot100/question031.java
@@ -6,4 +6,12 @@ package LeetCode_Hot100;
  * create 2026-01-26-22:01
  **/
 public class question031 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int f437(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        return 0;
+    }
 }
