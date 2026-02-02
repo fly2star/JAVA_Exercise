0264aeb2196c1ce0afdac448bc0a23c7c64dd37b
fly2star <2455741679@qq.com>
2026-02-02 21:06:42 +0800

    0202-3rd

--- src/LeetCode_Hot100/question036.java
+++ src/LeetCode_Hot100/question036.java
@@ -1,5 +1,18 @@
 package LeetCode_Hot100;
 
+/*
+347. 前 K 个高频元素
+
+给你一个整数数组 `nums` 和一个整数 `k`，请你返回其中出现频率前 `k` 高的元素。你可以按任意顺序返回答案。
+*/
 public class question036 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int f347(int[] nums, int K) {
+        
+        return 0;
+    }
     
 }
