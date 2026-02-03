2634cc797f38bbb492650126ee81317d381ef364
fly2star <2455741679@qq.com>
2026-02-03 16:24:41 +0800

    0203-2nd

--- src/LeetCode_Hot100/question003.java
+++ src/LeetCode_Hot100/question003.java
@@ -13,18 +13,19 @@ public class question003 {
     public static void main(String[] args) {
         ListNode node1 = new ListNode(2, null);
         ListNode node = new ListNode(1, node1);
-        System.out.println(f234(node));
+        System.out.println(isPalindrome(node));
 
     }
 
-    public static boolean f234(ListNode head) {
+    // 方法 1 : 
+    public static boolean isPalindrome(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode mid = slow;
+        // ListNode mid = slow;
 
         ListNode prev = null;
         while (slow != null) {
@@ -45,8 +46,23 @@ public class question003 {
         return true;
     }
 
+
+    // 方法 2 :
+    public static boolean isPalindrome2(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode head2 = reverseList(mid);
+        while (head2 != null) {
+            if (head.val != head2.val) {
+                return false;
+            }
+            head = head.next;
+            head2 = head2.next;
+        }
+        return true;
+    }
+
     // 876. 链表的中间结点
-    private ListNode middleNode(ListNode head) {
+    private static ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null && fast.next != null) {
@@ -57,7 +73,7 @@ public class question003 {
     }
 
     // 206. 反转链表
-    private ListNode reverseList(ListNode head) {
+    private static ListNode reverseList(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;
         while (cur != null) {
--- src/LeetCode_Hot100/question036.java
+++ src/LeetCode_Hot100/question036.java
@@ -1,5 +1,9 @@
 package LeetCode_Hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /*
 347. 前 K 个高频元素
 
@@ -7,12 +11,42 @@ package LeetCode_Hot100;
 */
 public class question036 {
     public static void main(String[] args) {
-        
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int K = 2;
+        int[] res = topKFrequent(nums, K);
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
     }
 
-    public static int topKFrequent(int[] nums, int K) {
-        
-        return 0;
+    public static int[] topKFrequent(int[] nums, int K) {
+        // 使用 Map 统计频率
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // 使用最小推 (按频率排序)
+        // new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue)) 谁的 value 值最小, 谁就在堆顶
+        // 在 Java 的 PriorityQueue 中，默认的排序逻辑是自然升序（从小到大）, 当比较器判定 A<B 时，A 会被放在堆顶。
+        // 写法 1
+        // PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        // 写法 2
+        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a , b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer,Integer> entry : freqMap.entrySet()) {
+            heap.offer(entry);
+            if (heap.size() > K) {
+                heap.poll();
+            }
+        }
+
+        // 提取结果
+        int[] result = new int[K];
+        for (int i = 0; i < K; i++) {
+            result[i] = heap.poll().getKey();
+        }
+
+        return result;
     }
     
 }
