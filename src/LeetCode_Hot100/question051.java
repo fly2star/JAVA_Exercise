eb252c790897c4a39b1554718507a014e61b413f
fly2star <2455741679@qq.com>
2026-03-04 18:54:06 +0800

    0304-1st

--- src/LeetCode_Hot100/question051.java
+++ src/LeetCode_Hot100/question051.java
@@ -1,5 +1,41 @@
 package LeetCode_Hot100;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+22. 括号生成
+
+数字 `n` 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 **有效的** 括号组合。
+
+*/
 public class question051 {
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0 , 0, n);
+        return result;
+    }
+
+    private static void backtrack(List<String> result, String current, int left, int right, int n) {
+        // 如果当前字符串长度达到 2n, 说明找到一个有效组合
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        // 可以添加左括号的条件: 左括号数量小于 n
+        if (left < n) {
+            backtrack(result, current + "(", left + 1, right, n);
+        }
+
+        // 可以添加右括号的条件: 右括号数量小于左括号数量
+        if (right < left) {
+            backtrack(result, current + ")", left, right + 1, n);
+        }
+    }
     
 }
--- src/LeetCode_Hot100/question053.java
+++ src/LeetCode_Hot100/question053.java
@@ -1,5 +1,100 @@
 package LeetCode_Hot100;
 
+/*
+48. 旋转图像
+
+给定一个 $ n \times n $ 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+*/
 public class question053 {
-    
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for (int[] is : matrix) {
+            for (int x : is) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("======================");
+        rotate(matrix);
+        for (int[] is : matrix) {
+            for (int x : is) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("======================");
+        rotate2(matrix);
+        for (int[] is : matrix) {
+            for (int x : is) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // 方法1: 借用一个数组来存储元素
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        int[] nums = new int[n * n];
+        int current = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                nums[current++] = matrix[j][i];
+            }
+        }
+        
+        current = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = nums[current++];
+            }
+        }
+
+    }
+
+    // 方法2: 转置 + 反转
+    public static void rotate2(int[][] matrix) {
+        int n = matrix.length;
+
+        // 转置矩阵
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 反转每一行
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 -j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    // 方法3: 直接旋转 (四元素交换)
+    public static void rotate3(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - 1 - j; j++) {
+                // 保存左上角元素
+                int temp = matrix[i][j];
+                // 左上角 = 左下角
+                matrix[i][j] = matrix[n - 1 - j][i];
+                // 左下角 = 右下角
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                // 右下角 = 右上角
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                // 右上角 = 左上角（保存的temp）
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+
+    }
 }
