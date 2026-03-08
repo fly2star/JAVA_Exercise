121d283105a537336639834121f892d209290bf6
fly2star <2455741679@qq.com>
2026-03-08 19:33:18 +0800

    0308-2nd

--- src/LeetCode_Hot100/question0057.java
+++ src/LeetCode_Hot100/question0057.java
@@ -1,5 +1,55 @@
 package LeetCode_Hot100;
 
+/*
+543. 二叉树的直径
+
+给你一棵二叉树的根节点，返回该树的 **直径**。
+
+二叉树的 **直径** 是指树中任意两个节点之间最长路径的长度。这条路径可能经过也可能不经过根节点 `root`。
+两节点之间路径的长度由它们之间边数表示。
+
+## 提示：
+    -- 树中节点数目在范围 [1, 10^4] 内
+    -- -100 ≤ Node.val ≤ 100
+*/
 public class question0057 {
-    
+
+    private static int maxDiameter = 0;
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        TreeNode ll = new TreeNode(4);
+        TreeNode lr = new TreeNode(5);
+        root.left.left = ll;
+        root.left.right = lr;
+        
+        System.out.println(diameterOfBinaryTree(root));
+    }
+
+    // 方法1: DFS
+    // 核心思想: 二叉树的直径 = 某个节点的左子树深度 + 右子树深度 的最大值
+    public static int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    private static int maxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = maxDepth(node.left);
+        int right = maxDepth(node.right);
+
+        // 更新全局最大直径
+        maxDiameter = Math.max(maxDiameter, left + right);
+
+        // 返回当前字数的深度(用于上层计算)
+
+        return Math.max(left, right) + 1; 
+    }
 }
--- src/LeetCode_Hot100/question058.java
+++ src/LeetCode_Hot100/question058.java
@@ -1,5 +1,82 @@
 package LeetCode_Hot100;
 
+/*
+34. 在排序数组中查找元素的第一个和最后一个位置
+
+给你一个按照非递减顺序排列的整数数组 `nums`，和一个目标值 `target`。请你找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 `target`，返回 `[-1, -1]`。
+
+你必须设计并实现时间复杂度为 \( O(\log n) \) 的算法解决此问题。
+
+## 提示：
+    -- 0 ≤ nums.length ≤ 10^5
+    -- -10^9 ≤ nums[i] ≤ 10^9
+    -- nums 是一个非递减数组
+    -- -10^9 ≤ target ≤ 10^9
+
+*/
 public class question058 {
-    
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        int[] res = searchRange(nums, target);
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+
+        res[0] = findLeft(nums, target);
+        res[1] = findRight(nums, target);
+        return res;
+    }
+
+    private static int findLeft(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                // 继续向左搜索
+                right = mid - 1;
+            }
+        }
+
+        // 检查 left 是否越界且等于 target
+        if (left >= 0 && nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+
+    private static int findRight(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                // 继续向右搜索
+                left = mid + 1;
+            }
+        }
+
+        // 检查 right 是否越界且等于 target
+        if (right >= 0 && nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
 }
