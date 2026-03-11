597a13eeba01d29c0fa83867b228e40c90a34196
fly2star <2455741679@qq.com>
2026-03-11 09:54:17 +0800

    0310-2nd

--- src/LeetCode_Hot100/question062.java
+++ src/LeetCode_Hot100/question062.java
@@ -18,11 +18,72 @@ package LeetCode_Hot100;
     -- 给定的树为二叉搜索树
 */
 public class question062 {
+
+    private int total = 0;
+
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        TreeNode lNode = new TreeNode(1);
+        TreeNode rNode = new TreeNode(6);
+        root.left = lNode;
+        root.right = rNode;
+        TreeNode llNode = new TreeNode(0);
+        TreeNode lrNode = new TreeNode(2);
+        lNode.left = llNode;
+        lNode.right = lrNode;
+        TreeNode rlNode = new TreeNode(5);
+        TreeNode rrNode = new TreeNode(7);
+        rNode.left = rlNode;
+        rNode.right = rrNode;
+        TreeNode lrrNode = new TreeNode(3);
+        lrNode.right = lrrNode;
+        TreeNode rrrNode = new TreeNode(8);
+        rrNode.right = rrrNode;
+
+        question062 sl = new question062();
+        sl.convertBST(root);
+        sl.printTree(root, 0);
+        
         
     }
 
     public TreeNode convertBST(TreeNode root) {
-        return null;
+        dfs(root);
+        return root;
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        // 先遍历右子树
+        dfs(node.right);
+
+        // 处理当前节点
+        total += node.val;
+        node.val = total;
+
+        // 再遍历左子树
+        dfs(node.left);
+
+    }
+
+    public void printTree(TreeNode root, int level) {
+        if (root == null) return;
+
+        // 1. 先处理右子树（在上方显示）
+        printTree(root.right, level + 1);
+
+        // 2. 打印当前节点，根据层级进行缩进
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) System.out.print("|\t");
+            System.out.println("|-------" + root.val);
+        } else {
+            System.out.println(root.val);
+        }
+
+        // 3. 处理左子树（在下方显示）
+        printTree(root.left, level + 1);
     }
 }
--- src/LeetCode_Hot100/question63.java
+++ src/LeetCode_Hot100/question63.java
@@ -1,5 +1,129 @@
 package LeetCode_Hot100;
 
+import java.util.PriorityQueue;
+
+/*
+23. 合并 K 个升序链表
+
+给你一个链表数组，每个链表都已经按升序排列。请你将所有链表合并到一个升序链表中，返回合并后的链表。
+*/
 public class question63 {
-    
+    public static void main(String[] args) {
+        ListNode[] lists = new ListNode[3];
+        ListNode lt0 = new ListNode(1, new ListNode(4, new ListNode(5, null)));
+        ListNode lt1 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+        ListNode lt2 = new ListNode(2, new ListNode(6, null));
+        lists[0] = lt0;
+        lists[1] = lt1;
+        lists[2] = lt2;
+        
+    }
+
+    // 方法1: 分治合并
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = left + (right - left) / 2;
+        // 分: 左边合好, 右边合好
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        // 治: 合并了两个有序链表
+        return mergeTwoLists(l1, l2);
+
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        curr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+    // 方法2: 顺序合并
+    public ListNode mergeKList2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode res = null;
+        for (ListNode list : lists) {
+            res = mergeTwoLists2(res, list);
+        }
+        return res;
+        
+    }
+
+    private ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+
+    // 方法3: 优先队列
+    public ListNode mergeKLists3(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        // 创建最小堆, 比较节点值
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a,b) -> a.val - b.val);
+
+        // 将所有链表的头节点加入堆
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (!heap.isEmpty()) {
+            ListNode node = heap.poll();
+            curr.next = node;
+            curr = curr.next;
+
+            if (node.next != null) {
+                heap.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+
+
 }
