facbb95179c930db492e0e1e6986743db68125d7
fly2star <2455741679@qq.com>
2026-03-11 22:11:19 +0800

    0311-4th

--- src/LeetCode_Hot100/question069.java
+++ src/LeetCode_Hot100/question069.java
@@ -1,5 +1,77 @@
 package LeetCode_Hot100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+15. 三数之和
+
+给你一个整数数组 `nums`，判断是否存在三元组 `[nums[i], nums[j], nums[k]]` 
+满足 `i != j`、`i != k` 且 `j != k`，同时还满足 `nums[i] + nums[j] + nums[k] == 0`。
+请你返回所有和为 0 且不重复的三元组。
+
+## 提示：
+    -- 3 ≤ nums.length ≤ 3000
+    -- -10^5 ≤ nums[i] ≤ 10^5
+*/
 public class question069 {
-    
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        question069 sl69 = new question069();
+        List<List<Integer>> result = sl69.threeSum(nums);
+        System.out.println(result);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n =nums.length;
+
+        // 排序: 这是双指针和去重的基础
+        Arrays.sort(nums);
+
+        // 固定 nums[i], 然后用另外两个指针扫描
+        for (int i = 0; i < n - 2; i++) {
+            // 如果当前数大于0，后面三个数之和必然大于0，直接退出
+            if (nums[i] > 0) {
+                break;
+            }
+
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // 跳过重复的左指针
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // 跳过重复的右指针
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        left--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            
+        }
+
+        return res;
+    }
 }
