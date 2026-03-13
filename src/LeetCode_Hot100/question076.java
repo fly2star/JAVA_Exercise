bd887005408b15676ee09fa4162ea39d9ac0a860
fly2star <2455741679@qq.com>
2026-03-13 16:06:58 +0800

    0313-2nd

--- src/LeetCode_Hot100/question076.java
+++ src/LeetCode_Hot100/question076.java
@@ -1,5 +1,129 @@
 package LeetCode_Hot100;
 
+/*
+79. 单词搜索
+
+给定一个 `m x n` 二维字符网格 `board` 和一个字符串单词 `word`。
+如果 `word` 存在于网格中，返回 `true`；否则，返回 `false`。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+同一个单元格内的字母不允许被重复使用。
+
+## 提示：
+    -- m == board.length
+    -- n = board[i].length
+    -- 1 ≤ m, n ≤ 6
+    -- 1 ≤ word.length ≤ 15
+    -- board 和 word 仅由大小写英文字母组成
+*/
 public class question076 {
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board.length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // 每一个格子都有可能是单词的起点
+                if (backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // 深度优先搜索
+    public boolean backtrack(char[][] board, String word, int r, int c, int index) {
+        // 成功匹配：索引到了单词末尾
+        if (index == word.length()) {
+            return true;
+        }
+
+        // 边界检查
+        if (r < 0 || r >= board.length || c < 0 || c > board[0].length) {
+            return false;
+        }
+
+        // 字符不匹配
+        if (board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+        // 做出选择: 标记当前格子为已访问
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        // 递归: 向四个方向搜索
+        boolean found = backtrack(board, word, r + 1, c, index + 1) ||
+                        backtrack(board, word, r - 1, c, index - 1) ||
+                        backtrack(board, word, r, c + 1, index + 1) ||
+                        backtrack(board, word, r, c - 1, index + 1);
+
+        // 撤销选择: 恢复现场
+        board[r][c] = temp;
+
+        return found;
+        
+
+    }
+}
+
+
+// 将变量作为类的成员变量
+class Solution076 {
+    private int m, n;
+    private char[][] board;
+    private String word;
+
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.word = word;
+        this.m = board.length;
+        this.n = board[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     
+    private boolean dfs(int i, int j, int index) {
+        // 找到完整单词
+        if (index == word.length()) {
+            return true;
+        }
+        
+        // 边界检查
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return false;
+        }
+        
+        // 字符不匹配
+        if (board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        // 标记已访问
+        char temp = board[i][j];
+        board[i][j] = '#';
+        
+        // 四个方向搜索
+        boolean found = dfs(i + 1, j, index + 1) ||
+                       dfs(i - 1, j, index + 1) ||
+                       dfs(i, j + 1, index + 1) ||
+                       dfs(i, j - 1, index + 1);
+        
+        // 回溯
+        board[i][j] = temp;
+        
+        return found;
+    }
 }
--- src/LeetCode_Hot100/question077.java
+++ src/LeetCode_Hot100/question077.java
@@ -1,5 +1,87 @@
 package LeetCode_Hot100;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+114. 二叉树展开为链表
+
+给你二叉树的根结点 `root`，请你将它展开为一个单链表：
+
+- 展开后的单链表应该同样使用 `TreeNode`，其中 `right` 子指针指向链表中下一个结点，而左子指针始终为 `null`。
+- 展开后的单链表应该与二叉树 **先序遍历** 顺序相同。
+
+
+## 提示：
+    -- 树中结点数在范围 [0, 2000] 内
+    -- -100 ≤ Node.val ≤ 100
+*/
 public class question077 {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    // 方法1: 递归 + 列表存储
+    /*
+    1. 对二叉树进行前序遍历，将节点按顺序存入列表
+    2. 遍历列表，将每个节点的左子节点设为 null，右子节点设为列表中的下一个节点
+    */
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        List<TreeNode> nodes = new ArrayList<>();
+        preorder(root, nodes);
+
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes.get(i).left = null;
+            nodes.get(i).right = nodes.get(i + 1);
+        }
+
+    }
+
+    private void preorder(TreeNode node, List<TreeNode> nodes) {
+        if (node == null) {
+            return;
+        }
+        nodes.add(node);
+        preorder(node.left, nodes);
+        preorder(node.right, nodes);
+    }
+
+    // 方法2: 原地算法 (O(1) 空间)
+    /*
+    1. 从根节点开始遍历：
+        - 如果当前节点有左子树：
+            - 找到左子树的最右节点（即左子树中先序遍历的最后一个节点）
+            - 将当前节点的右子树接到这个最右节点的右边
+            - 将当前节点的左子树移到右边，左子节点设为 null
+        - 移动到当前节点的右子节点继续处理
+    2. 重复直到所有节点处理完毕
+    */
+    public void flatten2(TreeNode root) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (curr.left != null) {
+                // 找到左子树的最右节点
+                TreeNode rightmost = curr.left;
+                while (rightmost.right != null) {
+                    rightmost = rightmost.right;
+                }
+
+                // 将原右子树接到最右节点右边
+                rightmost.right = curr.right;
+
+                // 将左子树移到右边, 左子节点设为空
+                curr.right = curr.left;
+                curr.left = null;
+            }
+
+            // 继续处理下一个节点
+            curr = curr.right;
+        }
+
+    }
 }
