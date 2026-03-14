9af0aeed7979137225a3f849a3ce17b8f018f75f
fly2star <2455741679@qq.com>
2026-03-14 10:47:15 +0800

    0313-5th

--- src/LeetCode_Hot100/question0083.java
+++ src/LeetCode_Hot100/question0083.java
@@ -1,5 +1,55 @@
 package LeetCode_Hot100;
 
+/*
+101. 对称二叉树
+
+给你一个二叉树的根节点 `root`，检查它是否轴对称。
+
+## 提示：
+    -- 树中节点数目在范围 [1, 1000] 内
+    -- -100 ≤ Node.val ≤ 100
+*/
 public class question0083 {
-    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1,
+                                        new TreeNode(2,
+                                            new TreeNode(3),
+                                            new TreeNode(4)),
+                                        new TreeNode(2,
+                                            new TreeNode(4), 
+                                            new TreeNode(3)));
+        
+        question0083 sl83 = new question0083();
+        System.out.println(sl83.isSymmetric(root));
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode leftNode, TreeNode rightNode) {
+        // 如果左右节点都为空，返回 true
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        
+        // 如果其中一个为空，返回 false
+        if (leftNode == null || rightNode == null) {
+            return false;
+        }
+
+        // 如果值不相等，返回 false
+        if (leftNode.val != rightNode.val) {
+            return false;
+        }
+
+        // 递归判断
+        return isMirror(leftNode.left, rightNode.right) && isMirror(leftNode.right, rightNode.left);
+
+
+        
+    }
 }
--- src/LeetCode_Hot100/question082.java
+++ src/LeetCode_Hot100/question082.java
@@ -1,5 +1,63 @@
 package LeetCode_Hot100;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+102. 二叉树的层序遍历
+
+给你二叉树的根节点 `root`，返回其节点值的 **层序遍历**。
+（即逐层地，从左到右访问所有节点）。
+
+## 提示：
+    -- 树中节点数目在范围 [0, 2000] 内
+    -- -1000 ≤ Node.val ≤ 1000
+*/
 public class question082 {
-    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3,
+                                    new TreeNode(9),
+                                    new TreeNode(20, 
+                                        new TreeNode(15),
+                                        new TreeNode(7))
+        );
+
+        question082 sl82 = new question082();
+        System.out.println(sl82.levelOrder(root));
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelList = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            res.add(levelList);
+
+        }
+
+        return res;
+
+    }
 }
