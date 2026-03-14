29f678303ee61230948737f73ac160cab2588be9
fly2star <2455741679@qq.com>
2026-03-14 20:43:01 +0800

    0314-3rd

--- src/LeetCode_Hot100/question88.java
+++ src/LeetCode_Hot100/question88.java
@@ -1,5 +1,7 @@
 package LeetCode_Hot100;
 
+import java.util.Stack;
+
 /*
 84. 柱状图中最大的矩形
 
@@ -12,5 +14,34 @@ package LeetCode_Hot100;
     -- 0 ≤ heights[i] ≤ 10^4
 */
 public class question88 {
-    
+    public static void main(String[] args) {
+        int[] heights = new int[]{2,1,5,6,2,3};
+        
+        question88 sl88 = new question88();
+        System.out.println(sl88.largestRectangleArea(heights));
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        // 创建一个新数组, 末尾添加一个 0 作为哨兵
+        int[] newHeights = new int[n + 1];
+        System.arraycopy(heights, 0, newHeights, 0, n);
+        newHeights[n] = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            // 当当前高度小于栈顶高度时，说明找到了右边界
+            while (!stack.isEmpty() && newHeights[stack.peek()] > newHeights[i]) {
+                int height = newHeights[stack.pop()];
+                // 计算宽度：如果栈为空，宽度为 i；否则宽度为 i - stack.peek() - 1
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        
+        return maxArea;
+    }
 }
