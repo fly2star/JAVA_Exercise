200305bd4e27d317dcfb016793143be305aa7916
fly2star <2455741679@qq.com>
2026-03-14 18:59:55 +0800

    0314-1st

--- src/LeetCode_Hot100/question084.java
+++ src/LeetCode_Hot100/question084.java
@@ -1,5 +1,82 @@
 package LeetCode_Hot100;
 
+/*
+98. 验证二叉搜索树
+
+给你一个二叉树的根节点 `root`，判断其是否是一个有效的二叉搜索树。
+
+## 有效二叉搜索树定义如下：
+    -- 节点的左子树只包含 **严格小于** 当前节点的数
+    -- 节点的右子树只包含 **严格大于** 当前节点的数
+    -- 所有左子树和右子树自身必须也是二叉搜索树
+
+## 提示：
+    -- 树中节点数目在范围 [1, 10^4] 内
+    -- -2^31 ≤ Node.val ≤ 2^31 - 1
+*/
 public class question084 {
-    
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(0,
+                                    new TreeNode(1),
+                                    new TreeNode(4,
+                                        new TreeNode(3),
+                                        new TreeNode(6)));
+                                        
+        question084 sl84 = new question084();
+        System.err.println(sl84.isValidBST(root1));
+    }
+
+    // 方法1: 递归边界
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);    
+    }
+
+    private boolean validate(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+
+        // 检查当前节点是否在范围内
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+
+        // 递归检查左右子树
+        return validate(node.left, lower, node.val) &&
+                validate(node.right, node.val, upper);
+    }
+
+    // 方法2: 中序遍历
+    /**
+     * 1. 对二叉树进行中序遍历
+     * 2. 在遍历过程中，记录前一个节点的值
+     * 3. 如果当前节点的值小于等于前一个节点的值，说明不是二叉搜索树
+     * 4. 遍历结束后返回 true
+    */
+    private long prev = Long.MIN_VALUE;
+
+    public boolean isValidBST2(TreeNode root) {
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode node) {
+        if (node == null) {
+            return true;
+        }
+
+        // 检查左子树
+        if (!inorder(node.left)) {
+            return false;
+        }
+
+        // 检查当前节点
+        if (node.val <= prev) {
+            return false;
+        }
+        prev = node.val;
+
+        // 检查右子树
+        return inorder(node.right);
+
+    }
 }
--- src/LeetCode_Hot100/question085.java
+++ src/LeetCode_Hot100/question085.java
@@ -1,5 +1,60 @@
 package LeetCode_Hot100;
 
+/*
+96. 不同的二叉搜索树
+
+给你一个整数 `n`，求恰由 `n` 个节点组成且节点值从 1 到 `n` 互不相同的 **二叉搜索树** 有多少种？
+返回满足题意的二叉搜索树的种数。
+*/
 public class question085 {
-    
+    public static void main(String[] args) {
+        
+        question085 sl85 = new question085();
+        System.out.println(sl85.numTrees(1));
+        System.out.println(sl85.numTrees2(3));
+    }
+
+    // 方法1: 动态规划
+    public int numTrees(int n) {
+
+        // dp[i] 表示 i 个节点能组成的 BST 数量
+        int[] dp = new int[n + 1];
+        dp[0] = 1;  // 空树
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                // 以 j 为根, 左子树有 j-1 个节点, 右子树有 i-j 个节点
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+
+    // 方法2: 数学方法 (卡特兰数)
+    /**
+     * 设 `G(n)` 表示 `n` 个节点能组成的 BST 数量。
+     * 则:
+     *      G(n)=\sum_{i=1}^{n} G(i-1) \times G(n-i)
+     *          G(0)=1  (空树算一种)
+     *          G(1)=1
+     * 这正是 卡特兰数 的递推公式!
+     * 
+     *      卡特兰数第 n 项:
+     *                  C_{n}=\dfrac{1}{n+1} \binom{2n}{n}
+     * 
+     * 利用卡特兰数通项公式:
+     *      C_{n} = \dfrac{(2n)!}{(n+1)!n!}=\prod_{i=0}^{n-1}\dfrac{i+2}{2(2i+1)}
+     * 
+    */
+    public int numTrees2(int n) {
+        long catalan = 1;
+        for (int i = 0; i < n; i++) {
+            catalan = catalan * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int)catalan;
+    }
+
+
 }
